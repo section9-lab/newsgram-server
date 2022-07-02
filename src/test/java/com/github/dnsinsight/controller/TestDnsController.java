b578edaaf6081dd5a7d8f29e0d1134d12857e8c5
Tachikoma <section9lab@gmail.com>
2022-07-02 21:37:34 +0800

    test: add find dns-info test

--- src/test/java/com/github/dnsinsight/controller/TestDnsController.java
+++ src/test/java/com/github/dnsinsight/controller/TestDnsController.java
@@ -10,7 +10,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
