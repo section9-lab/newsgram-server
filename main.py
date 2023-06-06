cd1454de012ee5902724dbd486dfc26c8c665737
Tachikoma <section9lab@gmail.com>
2023-06-06 15:57:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from datetime import datetime
+import whois
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Now Time:" }
+    res = {
+        "time": datetime.now(),
+        "url": "www.baidu.com",
+        "whois": whois.whois("www.baidu.com")
+    }
+    return res
