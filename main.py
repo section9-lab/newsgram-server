9bbb9580adaa8d524f55e5a2666b7b47672fa6ac
Tachikoma <section9lab@gmail.com>
2023-06-06 16:22:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ app = FastAPI()
 async def root():
     res = {
         "time": datetime.now(),
-        "url": "www.baidu.com",
-        "whois": whois.whois("www.baidu.com")
+        "des": "this is home page"
+    }
+    return res
+
+@app.get("/url/{item_url}")
+async def get_url(item_url):
+    if item_url == None:
+        item_url = "www.baidu.com"
+    res = {
+        "time": datetime.now(),
+        "url": item_url,
+        "whois": whois.whois(item_url)
     }
     return res
